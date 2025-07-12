34129691da72048edb3df5a1629317d808b33c73
Soham Sarkar <sarkarsoham07@gmail.com>
2025-07-12 07:40:24 +0530

    port change

--- app.py
+++ app.py
@@ -221,5 +221,5 @@ def index():
     return 'Portfolio Chatbot API up & running!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 9001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
